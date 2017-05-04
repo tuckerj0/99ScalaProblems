282e53405f5b2da7b3f22be2d4b0eaa28dbf5c2c
tuckerj0 <jat134@pitt.edu>
2017-05-03 20:19:49 -0400

    build

---
+++ src/test/scala/firstGo/lists/P01test.scala
@@ -0,0 +1,10 @@
+import org.scalatest.{FunSpec, Matchers}
+
+class P01Test extends FunSpec with Matchers {
+
+  describe("P01 List builtin method") {
+    it("should find last element") {
+      println("testing p01")
+    }
+  }
+}
